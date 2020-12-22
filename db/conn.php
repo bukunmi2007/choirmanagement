9090e6f176e63a7e5cd8e218bb84c14ae336ae2e
bukunmi2007 <47277258+bukunmi2007@users.noreply.github.com>
2020-12-21 22:41:18 -0500

    Added remote mysql connection details

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,16 @@
 <?php
-    $hostname = '127.0.0.1';
-    $db = 'choirmanagement_db';
-    $user = 'root';
-    $pass = '';
+    // Development Connection
+    //$hostname = '127.0.0.1';
+    //$db = 'choirmanagement_db';
+    //$user = 'root';
+    //$pass = '';
+    //$charset = 'utf8mb4';
+
+    // Remote Database Connection
+    $hostname = 'remotemysql.com';
+    $db = 'c0Q3OAm0n3';
+    $user = 'c0Q3OAm0n3';
+    $pass = '7Pf1rSNa3h';
     $charset = 'utf8mb4';
 
     //connector like jbdc and the likes
