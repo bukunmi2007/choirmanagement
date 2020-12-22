6a3237db5e58ac28b2420312e230e98f2f689561
bukunmi2007 <47277258+bukunmi2007@users.noreply.github.com>
2020-12-22 00:27:07 -0500

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -7,18 +7,19 @@
     //$charset = 'utf8mb4';
 
     // Remote Database Connection
-    $hostname = 'remotemysql.com';
-    $db = 'c0Q3OAm0n3';
-    $user = 'c0Q3OAm0n3';
-    $pass = '7Pf1rSNa3h';
+    $hostname = 'remotemysql.com'; 
+    $db = 'VOUn72BLqe';
+    $user = 'VOUn72BLqe';
+    $pass = 'Pn8n4SqltS';
     $charset = 'utf8mb4';
 
     //connector like jbdc and the likes
 
     $dsn = "mysql:hostname=$hostname; dbname=$db; charset=$charset";
 
+    
     try {
-        $pdo = new PDO ($dsn, $user, $pass);
+        $pdo = new PDO($dsn, $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //echo "Hello Database";
 
