e79cbab1129ea29160f9b2e4cbea639db43da78f
bukunmi2007 <47277258+bukunmi2007@users.noreply.github.com>
2020-12-24 20:06:58 -0500

    new database connection details

--- db/conn.php
+++ db/conn.php
@@ -1,17 +1,17 @@
 <?php
     // Development Connection
-    $hostname = '127.0.0.1';
-    $db = 'choirmanagement_db';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
+    // $hostname = '127.0.0.1';
+    // $db = 'choirmanagement_db';
+    // $user = 'root';
+    // $pass = '';
+    // $charset = 'utf8mb4';
 
     // Remote Database Connection
-    //$hostname = 'remotemysql.com'; 
-    //$db = 'VOUn72BLqe';
-    //$user = 'VOUn72BLqe';
-    //$pass = 'Pn8n4SqltS';
-    //$charset = 'utf8mb4';
+    $hostname = 'remotemysql.com'; 
+    $db = '3DRVz64Fib';
+    $user = '3DRVz64Fib';
+    $pass = 'Zqt4oUfcAJ';
+    $charset = 'utf8mb4';
 
     //connector like jbdc and the likes
 
